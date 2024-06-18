49572bb8dad6f1dcfd57ba50334cd46361fe3df4
Michał Kiełkowski <mkielkowski01@gmail.com>
2024-06-18 02:34:33 +0200

    network addition

--- visualizers.py
+++ visualizers.py
@@ -20,11 +20,37 @@ def perform_tsne(
     return results
 
 
+# def perform_umap(
+#     data,
+#     random_seed=0,
+# ):
+#     mapper = UMAP(
+#         random_state=random_seed,
+#     )
+#     results = mapper.fit_transform(data)
+#     return results
+
+
+# def perform_pacmap(
+#     data,
+#     random_seed=0,
+# ):
+#     mapper = PaCMAP(
+#         random_state=random_seed,
+#     )
+#     results = mapper.fit_transform(data)
+#     return results
 def perform_umap(
     data,
+    n_components=2,
+    n_neighbors=15,
+    min_dist=0.1,
     random_seed=0,
 ):
     mapper = UMAP(
+        n_components=n_components,
+        n_neighbors=n_neighbors,
+        min_dist=min_dist,
         random_state=random_seed,
     )
     results = mapper.fit_transform(data)
@@ -33,9 +59,13 @@ def perform_umap(
 
 def perform_pacmap(
     data,
+    n_components=2,
+    n_neighbors=5,
     random_seed=0,
 ):
     mapper = PaCMAP(
+        n_components=n_components,
+        n_neighbors=n_neighbors,
         random_state=random_seed,
     )
     results = mapper.fit_transform(data)
